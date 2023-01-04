7c8bc9b000b8f28c6b10ee4d6b63c155bc948e16
ed <ed.17@colemanzoo.com>
2023-01-04 16:01:46 -0700

    submit Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentageIn = Console.ReadLine();
+
+        int percentage = int.Parse(percentageIn);
+        string letter = "";
+        if(percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if(percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if(percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if(percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if((percentage >= 60) && (percentage < 97))
+        {
+            int minor = percentage % 10 ;
+            if(minor >= 7)
+            {
+                letter += "+";
+            }
+            if(minor < 3)
+            {
+                letter += "-";
+            }
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if(percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
