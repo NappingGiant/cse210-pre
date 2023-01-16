c02872e2b665fbae6bd91cd0609f53a4d15807bc
ed <ed.17@colemanzoo.com>
2023-01-16 14:36:11 -0700

    complete W03 Abstraction Learning Activity

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,22 @@
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear;
+    public int _endYear;
+
+    public Job()
+    {
+    }
+
+    public void Display()
+    {
+        // Displays the job information in the format
+        // "Job Title (Company) StartYear-EndYear",
+        // for example: "Software Engineer (Microsoft) 2019-2022".
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
+
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Intel";
+        job1._jobTitle = "Loose Cannon";
+        job1._startYear = 2000;
+        job1._endYear = 2016;
+
+        Job job2 = new Job();
+        job2._company = "Providence Health System";
+        job2._jobTitle = "Troublemaker";
+        job2._startYear = 1995;
+        job2._endYear = 2000;
+
+        Resume myResume = new Resume();
+        myResume._name = "Mortimer Schnerd";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
