88acf10f2f734f751bcbf117be52e0ff489253e6
ed <ed.17@colemanzoo.com>
2023-01-23 18:36:14 -0700

    20230123

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,16 +1,27 @@
-
-
 using System.IO; 
 public class Journal
 {
-    public List<JournalEntry> journalEntries = new List<JournalEntry>();
+    // make a place to hold the journal entries
+    List<JournalEntry> journalEntries = new List<JournalEntry>();
 
+    // the Prompt class has a list of prompts to use
     Prompt prompt = new Prompt();
 
+    // information line prefix
+    //const string lp = ". ";
+    string lp = "│ ";
+
+    // constructor (not used)
     public Journal()
     {
     }
 
+    /*
+        get a random prompt and display it on the console
+        read the user response from the console
+        if there's an empty response, just return
+        if there's a response, create a journal entry and add it to the journal
+    */
     public void AddEntry()
     {
         // create a working JournalEntry
@@ -19,8 +30,9 @@ public class Journal
         // get a random prompt
         entry._prompt = prompt.Get();
 
-        // prompt and get the response
+        // prompt for and get the response
         Console.WriteLine(entry._prompt);
+        Console.Write("> ");
         entry._response = Console.ReadLine();
 
         // if there is a response, add the entry to the journal
@@ -31,32 +43,38 @@ public class Journal
             entry._entryDate = theCurrentTime.ToShortDateString();
 
             journalEntries.Add(entry);
-            Console.WriteLine("* Entry Added\n");
+            Console.WriteLine($"\n{lp}journal entry Added\n{lp}");
         }
-        else
+        else // empty response, so just exit
         {
-            Console.WriteLine("* Entry NOT Added due to no response\n");
+            Console.WriteLine($"\n{lp}journal entry NOT Added due to no response\n{lp}");
         }
     }
 
+    /*
+        loop through the responses in the journal, displaying each one
+    */
     public void DisplayAll()
     {
-        Console.WriteLine($"Showing {journalEntries.Count} journal entries:\n");
+        //Console.WriteLine($"{lp}showing {journalEntries.Count} journal entries:\n");
+        Console.WriteLine($"{lp}Number of journal entries to show: {journalEntries.Count}\n");
         foreach(JournalEntry entry in journalEntries)
         {
-            entry.Display();
+            entry.Display(); // use the JournalEntry's Display method
         }
-
-        Console.WriteLine("End of Journal\n");
-
+        Console.WriteLine($"{lp}end of journal\n");
     }
 
+    /*
+        TODO: prompt for a file name
+        save the journal entries to a text file, one per line
+    */
     public void SaveToFile()
     {
         //TODO: prompt for fileName
         string fileName = "journal.txt";
 
-        Console.WriteLine($"Ok, saving the journal to {fileName}");
+        Console.WriteLine($"{lp}Ok, saving the journal to {fileName}");
         using (StreamWriter outputFile = new StreamWriter(fileName))
         {
             foreach(JournalEntry entry in journalEntries)
@@ -64,21 +82,26 @@ public class Journal
                 outputFile.WriteLine($"{entry._entryDate}|{entry._prompt}|{entry._response}");
             }
         }
-        Console.WriteLine($"{journalEntries.Count} journal entries written\n");
+        Console.WriteLine($"{lp}{journalEntries.Count} journal entries written\n");
     }
 
+    /*
+        TODO: prompt for a file name
+        clear the existing journal entries
+        read journal entries from a file, one per line
+        parse each line and add an entry to the journal
+    */
     public void ReadFromFile()
     {
         //TODO: prompt for fileName
         string filename = "journal.txt";
 
         // remove any existing entries in the journal
-        Console.WriteLine("Clearing any existing entries in the journal");
+        Console.WriteLine($"{lp}Clearing any existing entries in the journal");
         journalEntries.Clear();
 
-        Console.WriteLine($"Reading a journal from {filename}");
+        Console.WriteLine($"{lp}Reading a journal from {filename}");
         string[] lines = System.IO.File.ReadAllLines(filename);
-
 
         foreach(string line in lines)
         {
@@ -92,7 +115,48 @@ public class Journal
             journalEntries.Add(entry);
         }
 
-        Console.WriteLine($"{journalEntries.Count} entries added to the journal\n");
+        Console.WriteLine($"{lp}{journalEntries.Count} entries added to the journal\n");
     }
 
+    /* 
+        display a list of choices on the console and wait for user response
+        act on the response by calling the appropriate method
+        loop back to display the list of choices
+    */
+    public void Menu()
+    {
+        Console.WriteLine("\nWelcome to the Journal Program :)");
+        string selection = "";
+        while(selection != "5")
+        {
+            Console.Write($"{lp}Here are the options:\n{lp} 1-Write\n{lp} 2-Display\n{lp} 3-Load\n{lp} 4-Save\n{lp} 5-Exit\n{lp}Your choice? ");
+            selection = Console.ReadLine();
+            Console.WriteLine();
+
+            if(selection == "1")
+            {
+                AddEntry();
+            }
+            else if(selection == "2")
+            {
+                DisplayAll();
+            }
+            else if(selection == "4")
+            {
+                SaveToFile();
+            }
+            else if(selection == "3")
+            {
+                ReadFromFile();
+            }
+            else if(selection == "5")
+            {
+                Console.WriteLine($"Thanks for playing. Bye!\n");
+            }
+            else
+            {
+                Console.WriteLine($"\n{lp}Huh? What is '{selection}' for?");
+            }
+        }
+    }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,41 +5,10 @@ class Program
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Develop02 World!");
+        //ConsoleOutputTest.ShowEm();
 
         Journal myJournal = new Journal();
+        myJournal.Menu();
 
-        string selection = "";
-        while(selection != "0")
-        {
-            Console.Write($"What to do, what to do\n 1-Add an entry\n 2-Show all entries\n 3-Save to a file\n 4-Read from a file\n 0-Exit\n (1, 2, 3, 4, 0) >");
-            selection = Console.ReadLine();
-            Console.WriteLine("");
-
-            if(selection == "1")
-            {
-                myJournal.AddEntry();
-            }
-            else if(selection == "2")
-            {
-                myJournal.DisplayAll();
-            }
-            else if(selection == "3")
-            {
-                myJournal.SaveToFile();
-            }
-            else if(selection == "4")
-            {
-                myJournal.ReadFromFile();
-            }
-            else if(selection == "0")
-            {
-                Console.WriteLine("Thanks for playing. Bye!\n");
-            }
-            else
-            {
-                Console.WriteLine($"\nHuh? What is '{selection}' for?");
-            }
-
-        }
     }
 }
