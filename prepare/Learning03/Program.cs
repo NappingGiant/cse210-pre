747ca96c9dbe7056f61f721aeee9d05c7785f7a1
ed <ed.17@colemanzoo.com>
2023-01-30 18:30:25 -0700

    Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction t1 = new Fraction();
+        Console.WriteLine($"{t1.GetFractionString()}\n{t1.GetDecimalValue()}\n");
+
+        Fraction t2 = new Fraction(5);
+        Console.WriteLine($"{t2.GetFractionString()}\n{t2.GetDecimalValue()}\n");
+
+        Fraction t3 = new Fraction(3, 4);
+        Console.WriteLine($"{t3.GetFractionString()}\n{t3.GetDecimalValue()}\n");
+
+        Fraction t4 = new Fraction(1, 3);
+        Console.WriteLine($"{t4.GetFractionString()}\n{t4.GetDecimalValue()}\n");
     }
 }
