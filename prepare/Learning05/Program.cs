f5dffe406be04da63e134fd4074be5c2926b8790
ed <ed.17@colemanzoo.com>
2023-03-01 15:18:46 -0700

    do Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Test();
+    }
+
+    static void Test()
+    {
+        List<Shape> _shapes = new List<Shape>();
+
+        _shapes.Add(new Square("Blue", 12));
+        _shapes.Add(new Rectangle("Green", 8, 4));
+        _shapes.Add(new Circle("Red", 10));
+
+        foreach(Shape shape in _shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} {shape.GetType()} has an area of {shape.GetArea()}");
+        }
+
     }
 }
